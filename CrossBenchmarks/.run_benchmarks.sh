60d7ab2387b1729015a9dae4f14bbe1c61f344dc
Ryan Newton <rrnewton@gmail.com>
2014-11-20 11:45:18 -0500

    Add initial, partial hsbencher harness

---
+++ CrossBenchmarks/.run_benchmarks.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "Begin running jenkins benchmark script for Pycket.  First use regression script to build packages:"
+set -x
+set -e
+
+echo "On linux platforms, check CPU affinity:"
+taskset -pc $$ || echo ok
+
+echo "Also check load:"
+sar 2 2 || echo ok
+
+echo "And who"
+who -a || echo ok
+
+# NOTEST=1 ./.jenkins_script.sh -j
+
+echo "\nReturned to benchmarking script."
+
+# CONVENTION: The working directory is passed as the first argument.
+CHECKOUT=$1
+shift || echo ok
+
+if [ "$CHECKOUT" == "" ]; then
+  CHECKOUT=`pwd`
+fi
+if [ "$JENKINS_GHC" == "" ]; then
+   echo "JENKINS_GHC unset"
+   export JENKINS_GHC=7.8.3
+fi
+if [ -f "$HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_ghc.sh" ]; then
+  source $HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_ghc.sh
+fi
+if [ -f "$HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_cuda.sh" ]; then
+  source $HOME/continuous_testing_setup/rn_jenkins_scripts/acquire_cuda.sh
+fi
+
+echo "Running benchmarks remotely on server `hostname`"
+
+set -e
+
+# Switch to where the benchmarks are
+# ----------------------------------------
+cd "$CHECKOUT"/
+
+# Fetch data and build benchmark runner:
+# make
+
+export TRIALS=3
+
+if [ "$MACHINECLASS" == "cutter" ]; then
+  # Using generic uploader because we're over limit:
+  # Generic 1:
+  CID=905767673358.apps.googleusercontent.com
+  SEC=2a2H57dBggubW1_rqglC7jtK
+elif [ "$MACHINECLASS" == "lh008" ]; then 
+  # Using generic uploader because we're over limit:
+  # Generic 2:
+  CID=546809307027-8tm2lp5gtqg5o3pn3s016gd6467cf7j3.apps.googleusercontent.com
+  SEC=148aQ08EPpgkb0DiYVLoT9X2
+else
+  # Generic 3:
+  CID=759282369766-ijonhc4662ot2qos4lgud0e0sltjshlj.apps.googleusercontent.com
+  SEC=yI8GfZXsHPrW44udqklCHeDH
+fi
+
+TABLENAME=Pycket_benchmarks
+
+if [ "$MACHINECLASS" == "" ]; then
+    export MACHINECLASS=`hostname -s`
+fi
+
+./run-pycket-benchmarks.exe --keepgoing --trials=$TRIALS --fusion-upload --name=$TABLENAME --clientid=$CID --clientsecret=$SEC --hostname=$MACHINECLASS $*
---
+++ CrossBenchmarks/run-benchmark.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- | HSBencher script to run all the benchmarks.
+
+module Main where
+
+import HSBencher
+import HSBencher.Backend.Fusion    (defaultFusionPlugin)
+-- import HSBencher.Backend.Codespeed (defaultCodespeedPlugin, CodespeedConfig(..))
+import HSBencher.Backend.Dribble   (defaultDribblePlugin)
+import HSBencher.Methods.Builtin   (makeMethod)
+
+import qualified Control.Monad.Trans as Trans
+
+import Data.Default (Default(def))
+import Data.Monoid (mappend)
+import qualified Data.Map as M
+import System.Environment (getEnvironment)
+import System.Directory   (setCurrentDirectory, getDirectoryContents, getCurrentDirectory)
+import System.IO.Unsafe   (unsafePerformIO)
+import System.Process     
+import GHC.Conc           (getNumProcessors)
+
+--------------------------------------------------------------------------------
+
+benches :: [Benchmark DefaultParamMeaning]
+benches = 
+  [ (mkBenchmark ("../bin/run-"++which) [mode, prog]
+      (And [ allBenchParams, Set (Variant (which++mode)) (RuntimeArg "") ]))
+     { progname = Just ("CrossBench-"++prog) }
+  | prog <- words "ack array1 browse cat conform cpstak ctak dderiv deriv destruc diviter divrec dynamic earley fft fib fibc fibfp gcbench gcold graphs lattice matrix maze mazefun mbrot nqueens nucleic paraffins parsing perm9 peval pi pnpoly primes puzzle ray scheme simplex slatex string sum sumfp sumloop tail tak takl trav1 trav2 triangl wc"
+--  , mode <- ["-nothing", "-fixflo", "-unsafe"]
+  , mode <- ["-nothing", "-fixflo" ]
+  , which <- ["racket", "pycket"] ]
+
+--    confidence_level: 0.95
+--     number_of_data_points: 10
+--    max_time: 60   # time in seconds
+
+--         #variable_values: [nothing, fixflo, unsafe]
+
+
+--------------------------------------------------------------------------------
+-- Param settings and sizes:
+--------------------------------------------------------------------------------
+
+allBenchParams :: BenchSpace DefaultParamMeaning
+allBenchParams = And []
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  putStrLn "Begin Pycket benchmarks..."
+  defaultMainModifyConfig $ \ conf ->
+    addPlugin defaultFusionPlugin fsconf $ 
+    addPlugin defaultDribblePlugin def $ 
+--    addPlugin defaultCodespeedPlugin csconf $ 
+    conf{ benchlist  = benches
+        , runTimeOut = Just 30
+        , buildMethods = [ shellMethod ]
+        , harvesters = customTagHarvesterDouble "RESULT-cpu"   `mappend`
+                       customTagHarvesterDouble "RESULT-gc"    `mappend`
+                       customTagHarvesterDouble "RESULT-total" `mappend`
+                       harvesters conf
+        }
+ where
+  -- Some default settings for benchmarking:
+  -- csconf = def { codespeedURL = "http://codespeed.crest.iu.edu"
+  --              , projName     = "LVishParInfer" }
+
+  -- By default we can bake settings in here rather than passing them
+  -- on the command line:
+  fsconf = def 
+-- CID=820162629229-kp29aklebt6ucos5a71u8tu3hu8unres.apps.googleusercontent.com
+-- SEC=pSsMxVAJCFKyWsazuxZVRZwX
+
+-- | Teach HSBencher how to run a shell script or already-built binary.
+shellMethod :: BuildMethod
+shellMethod = BuildMethod
+  { methodName = "shell-script-or-binary"
+  , canBuild = AnyFile
+  , concurrentBuild = False
+  , setThreads      = Nothing
+  , clean = \ pathMap _ target -> return ()
+  , compile = \ pathMap bldid flags target -> do
+--       _ <- Trans.lift $ system "raco pkg install gcstats"
+       let runit args envVars =
+             CommandDescr
+             { command = ShellCommand
+                         (target++" "++ unwords args)
+             , timeout = Just 30                         
+             , workingDir = Nothing
+             , envVars
+             , tolerateError = False
+             }
+       return (RunInPlace runit)
+  }
